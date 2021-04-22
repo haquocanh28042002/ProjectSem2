a8b003ca102a0bab282346126463bcda9dfb2cae
buingocmya <81601941+buingocmya@users.noreply.github.com>
2021-04-22 09:34:41 +0700

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,14 @@
 #include "console.h"
 #include "Menu.h"
 int main(){
-	menu(thaotac, 3);
-	system("pause");
+	menu(thaotac, 4);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    HANDLE hdlConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_FONT_INFOEX consoleFont;
+    consoleFont.cbSize = sizeof(consoleFont);
+    GetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
+    memcpy(consoleFont.FaceName, L"Consolas", sizeof(consoleFont.FaceName));
+    SetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
 	return 0;
 }
--- Menu.h
+++ Menu.h
@@ -1,13 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
 #include "console.h"
 #include <conio.h>
+#include "Course.h"
 #define MAUNEN 176
 #define MAUCHU 7
 using namespace std;
 
 
 typedef char str[31];
-str thaotac[3] = { "1.Add new","2.Delete","3.List" };
+str thaotac[4] = { "1.Add new","2.Delete","3.List","0.Exit" };
 enum TRANGTHAI { UP, DOWN, LEFT, RIGHT, ENTER, BACK };
 TRANGTHAI key(int z) {
 	if (z == 224) {
@@ -21,17 +22,19 @@ TRANGTHAI key(int z) {
 	else if (z == 13) return ENTER;
 }
 
-int menu(str thaotac[3], int n) {
+int menu(str thaotac[4], int n) {
+	clrscr();
 	int k = 0;
 	int tt = 0; //thao tacs thu nhat
 	int* mau = new int[n];
-	for (int i = 0; i > n; i++) mau[i] = MAUCHU;
+	for (int i = 0; i < n; i++) mau[i] = MAUCHU;
 	mau[0] = MAUNEN;
 	while (1)
 	{
 		//in giao dien menu
 		for (int i = 0; i < n; i++) {
 			TextColor(mau[i]);
+//			gotoXY(50, 10 + i);
 			cout << thaotac[i] << endl;
 		}
 
@@ -55,19 +58,52 @@ int menu(str thaotac[3], int n) {
 		mau[tt] = MAUNEN;
 		clrscr();
 		if (k == 1) {
+			int tmp=1;
 			clrscr();
 			if (tt == 0){
-				cout << "Chua lam" << endl;
+				addnewcourse();
+				wcout << "0. Back ";
+				wcin >> tmp;
+				if (tmp == 0) { menu(thaotac, 4); }
+				break;
 			}
 			if (tt == 1) {
-				cout << "Xoa Menu, chua lam" << endl;
+				/*  course* phead = nullptr;
+				inputlistcourse(phead);
+				xemlist(phead);
+				wstring t;
+				wcout << "Nhập ID môn học cần xóa: " << endl;
+				wcin >> t;
+				deletecourse(phead,t);
+				fstream f;
+				f.open("course.csv", ios::out||ios::trunc);
+				outputlistcourse(phead);
+				phead = nullptr;
+				inputlistcourse(phead);
+				clrscr();
+				xemlist(phead);
+				break;
+				f.close();*/
+				wcout << "Delete ID course: " << endl;
+
+				break;
 			}
 			if (tt == 2) {
-		
-				cout << "Xem Menu, chua lam" << endl;
+				course* phead = nullptr;
+				inputlistcourse(phead);
+				xemlist(phead);
+				wcout << "0. Back ";
+				wcin >> tmp;
+				if (tmp == 0) { menu(thaotac, 4); }
+				break;
 			}
-			break;
+			if (tt == 3) {
+				clrscr();
+				wcout << "See you again";
+				break;
+			}
 		}
 	}
+	return 0;
 }
 
