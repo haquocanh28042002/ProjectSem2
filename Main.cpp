dd7cc6a43e68aa7ff2c0453316c27cd918e125dd
buingocmya <81601941+buingocmya@users.noreply.github.com>
2021-05-05 09:35:43 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "console.h"
 #include "Menu.h"
+using namespace std;
 int main(){
     menu(thaotac, 4);
-    /*_setmode(_fileno(stdin), _O_U16TEXT);
+    
+    _setmode(_fileno(stdin), _O_U16TEXT);
     _setmode(_fileno(stdout), _O_U16TEXT);
     HANDLE hdlConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_FONT_INFOEX consoleFont;
@@ -11,6 +13,6 @@ int main(){
     GetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
     memcpy(consoleFont.FaceName, L"Consolas", sizeof(consoleFont.FaceName));
     SetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
-    */
+    
     return 0;
 }
