dee237ecea5a108c78f0d93d573d4d6553d2e9a0
buingocmya <81601941+buingocmya@users.noreply.github.com>
2021-05-08 16:46:02 +0700

    Add files via upload

---
+++ Course.h
@@ -0,0 +1,174 @@
+﻿#include <iostream>
+#include <io.h>
+#include<sstream>
+#include <fcntl.h>
+#include <string>
+#include<fstream>
+#include<iomanip>
+#include <windows.h>
+#include<codecvt>
+#include<locale>
+#include <stdlib.h>
+#include<stdio.h>
+
+using namespace std;
+
+
+struct date {
+	int d, m, y;
+};
+enum dayofweek {
+	MON = 2,
+	TUE, WED, THU, FRI, SAT, SUN
+};
+enum  session{
+	S1 = 1, S2, S3, S4
+};
+struct course {
+	wstring IDcourse, namecourse, teacher, credit, dow1, dow2, s1, s2;
+	course* pNext;
+};
+void addnewcourse();
+void inputlistcourse(course *&phead);
+void xemlist(course* phead);
+void deletecourse(course*& phead, wstring t);
+void outputlistcourse(course* phead);
+void inputlistcourse(course* &phead) {
+	wfstream f;
+	f.open(L"course.csv", ios_base::in);
+	f.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+	course* pcur = nullptr;
+	wstring IDcourse, namecourse, teacher;
+	wstring credit, dow1, dow2, s1, s2;
+	phead = new course;
+	pcur = phead;
+	getline(f, pcur->IDcourse, L',');
+	getline(f, pcur->namecourse, L',');
+	getline(f, pcur->teacher, L',');
+	getline(f, pcur->credit, L',');
+	getline(f, pcur->dow1, L',');
+	getline(f, pcur->s1, L',');
+	getline(f, pcur->dow2, L',');
+	getline(f, pcur->s2);
+	phead->pNext = nullptr;
+	while (!f.eof()) {
+		pcur->pNext = new course;
+		pcur = pcur->pNext;
+		getline(f, pcur->IDcourse, L',');
+		getline(f, pcur->namecourse, L',');
+		getline(f, pcur->teacher, L',');
+		getline(f, pcur->credit, L',');
+		getline(f, pcur->dow1, L',');
+		getline(f, pcur->s1, L',');
+		getline(f, pcur->dow2, L',');
+		getline(f, pcur->s2);
+		pcur->pNext = nullptr;
+	}
+	f.close();
+}
+void xemlist(course* phead) {
+	system("cls");
+	int x = 1, y = 1;
+	course* pcur = phead;
+	while (pcur != nullptr) {
+		//gotoXY(x, y);
+		wcout << pcur->IDcourse << " ";
+		wcout << pcur->namecourse << " ";
+		wcout << pcur->teacher << " ";
+		wcout << pcur->credit << " ";
+		wcout << pcur->dow1 << " ";
+		wcout << pcur->s1 << " ";
+		wcout << pcur->dow2 << " ";
+		wcout << pcur->s2 << endl;
+		pcur = pcur->pNext;
+	}
+	return;
+}
+void deletecourse(course*& phead, wstring t) {
+	course* pcur = phead;
+	if (pcur->IDcourse == t) {
+		course* tmp = phead;
+		phead = phead->pNext;
+		delete tmp;
+		tmp = nullptr;
+		return;
+	}
+	while (pcur->pNext != nullptr) {
+		if (pcur->pNext->IDcourse == t) {
+			course* tmp = pcur->pNext;
+			pcur->pNext = pcur->pNext->pNext;
+			delete tmp;
+			tmp = nullptr;
+		}
+		if (pcur->pNext == nullptr) {
+			course* tmp = pcur->pNext;
+			delete tmp;
+			tmp = nullptr;
+		}
+		else 
+			pcur = pcur->pNext;
+	}
+}
+void outputlistcourse(course* phead) {
+	wfstream f;
+	f.open("course.csv", ios::out);
+	std::locale loc(std::locale(), new std::codecvt_utf8<wchar_t>);
+	f.imbue(loc);
+	course* pcur = phead;
+	while (pcur != nullptr) {
+		f << pcur->IDcourse << L",";
+		f << pcur->namecourse << L",";
+		f << pcur->teacher << L",";
+		f << pcur->credit << L",";
+		f << pcur->dow1 << L",";
+		f << pcur->s1 << L",";
+		f << pcur->dow2 << L",";
+		f << pcur->s2 << endl;
+		pcur = pcur->pNext;
+	}
+	f.close();
+}
+void addnewcourse(){
+	std::locale loc(std::locale(), new std::codecvt_utf8<wchar_t>);
+	wfstream f;
+	f.imbue(loc);
+	course* a = nullptr;
+	a = new course;
+	a->pNext = nullptr;
+	f.open("course.csv", ios::app);
+
+	wcout << "ID course: ";
+	getline(std::wcin, a->IDcourse);
+	f << a->IDcourse<<",";
+
+	wcout << "Name course: ";
+	getline(std::wcin, a->namecourse);
+	f << a->namecourse<<",";
+
+	wcout << "Teach: ";
+	getline(std::wcin, a->teacher);
+	f << a->teacher << ",";
+
+	wcout<< "Number of credits: ";
+	getline(std::wcin, a->credit);
+	f << a->credit << ",";
+
+	wcout << "Time of session 1(MON/TUES/...): ";
+	getline(std::wcin, a->dow1);
+	f << a->dow1 << ",";
+
+	wcout << "S1/S2/S3/S4: ";
+	getline(std::wcin, a->s1);
+	f << a->s1 << ",";
+
+	wcout << "Time of session 2(MON/TUES/...): ";
+	getline(std::wcin, a->dow2);
+	f << a->dow2 << ",";
+
+	wcout << "S1/S2/S3/S4: ";
+	getline(std::wcin, a->s2);
+	f << a->s2 << endl;
+	 
+	delete a;
+}
+
---
+++ Main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include "console.h"
+#include "Menu.h"
+using namespace std;
+int main(){
+
+    _setmode(_fileno(stdout), _O_WTEXT); //needed for output
+    _setmode(_fileno(stdin), _O_WTEXT);
+    HANDLE hdlConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_FONT_INFOEX consoleFont;
+    consoleFont.cbSize = sizeof(consoleFont);
+    GetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
+    memcpy(consoleFont.FaceName, L"Consolas", sizeof(consoleFont.FaceName));
+    SetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
+   
+    menu(thaotac, 4);
+
+    return 0;
+}
